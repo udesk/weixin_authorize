888d90257957f3484415fd1d1216bfbda16644b7
wangmingle <mingle5566@163.com>
2018-11-07 16:39:39 +0800

    模拟开发者模式(游光)

--- lib/weixin_authorize/client.rb
+++ lib/weixin_authorize/client.rb
@@ -14,13 +14,17 @@ module WeixinAuthorize
     attr_accessor :app_id, :app_secret, :expired_at # Time.now + expires_in
     attr_accessor :access_token, :redis_key
     attr_accessor :storage
+    attr_accessor :disguise
+    attr_accessor :disguise_url
 
-    def initialize(app_id, app_secret, redis_key=nil)
+    def initialize(app_id, app_secret, disguise = false, disguise_url = nil, redis_key=nil)
       @app_id     = app_id
       @app_secret = app_secret
       @expired_at = Time.now.to_i
       @redis_key  = security_redis_key((redis_key || "weixin_" + app_id))
       @storage    = Storage.init_with(self)
+      @disguise   = disguise
+      @disguise_url = disguise_url
     end
 
     # return token
@@ -41,12 +45,25 @@ module WeixinAuthorize
 
       def http_get(url, headers={}, endpoint="plain")
         headers = headers.merge(access_token_param)
+        if @disguise && !@disguise_url.nil?
+          get_url = get_disguise_url(@disguise_url, headers)
+          return WeixinAuthorize.http_get_disguise_url(get_url, {})
+        end
         WeixinAuthorize.http_get_without_token(url, headers, endpoint)
       end
 
       def http_post(url, payload={}, headers={}, endpoint="plain")
         headers = access_token_param.merge(headers)
+        if @disguise && !@disguise_url.nil?
+          post_url = get_disguise_url(@disguise_url, headers)
+          return WeixinAuthorize.http_post_disguise_url(post_url, payload, {})
+        end
         WeixinAuthorize.http_post_without_token(url, payload, headers, endpoint)
+      end
+
+      def get_disguise_url(disguise_url, headers = {})
+        _url = headers.to_a.map{|x| "#{x[0]}=#{x[1]}" }.join("&")
+        disguise_url.include?('?') ? "#{disguise_url}&#{_url}" : "#{disguise_url}?#{_url}"
       end
 
       def security_redis_key(key)
